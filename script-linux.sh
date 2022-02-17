4d8562bb4c7859867de5d9bc0a3f4d9c14f59ac5
Zhenchi <zhongzc_arch@outlook.com>
2022-02-17 18:11:21 +0800

    clean
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- script-linux.sh
+++
@@ -1,1 +0,0 @@
-echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
--- setup-conda.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-sudo conda install -yc defaults -c conda-forge mamba
-sudo mamba install -y conda-build anaconda-client conda-verify
